3aaa1796694feca166c6cd39bd8b287af83305bd
fathallah2004 <fathallahamine2004@gmail.com>
2024-12-23 23:25:08 +0100

    creation des label

--- Project/src/Pack/UserSignup.java
+++ Project/src/Pack/UserSignup.java
@@ -1,15 +1,32 @@
-package Pack ; 
+package Pack; 
 
 import java.awt.event.*;
 import javax.swing.*;
 public class UserSignup extends JFrame implements ActionListener {
+    JPanel p = new JPanel() ;
+    JLabel name = new JLabel("name") ;
+    JLabel governement = new JLabel("governement") ;
+    JLabel email = new JLabel("email") ;
+    JLabel password = new JLabel("password") ;
+
+
 
     public UserSignup() {
         super("user sign up") ;
         setSize(500,250);
         setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        
         setResizable(false);
+        setLocationRelativeTo(null);
+        
+        p.add(name) ;
+        p.add(governement) ;
+        p.add(email) ;
+        p.add(password) ;
+        setContentPane(p) ;
+        setVisible(true);
+
+
 
         
         
