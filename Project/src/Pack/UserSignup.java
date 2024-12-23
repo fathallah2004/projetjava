6a96fe13a3c75dead0aad3f9de643f780accf6f0
fathallah2004 <fathallahamine2004@gmail.com>
2024-12-24 00:24:30 +0100

    this commit is foer the creation of the textfield and label and their positioning with GridBagContraint

--- Project/src/Pack/UserSignup.java
+++ Project/src/Pack/UserSignup.java
@@ -1,5 +1,8 @@
 package Pack; 
 
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.*;
 import javax.swing.*;
 public class UserSignup extends JFrame implements ActionListener {
@@ -8,7 +11,11 @@ public class UserSignup extends JFrame implements ActionListener {
     JLabel governement = new JLabel("governement") ;
     JLabel email = new JLabel("email") ;
     JLabel password = new JLabel("password") ;
-
+    GridBagConstraints gbc =new GridBagConstraints() ; 
+    JTextField nameField = new JTextField(20);
+    JTextField governementField = new JTextField(20);
+    JTextField emailField = new JTextField(20);
+    JTextField passwordField = new JTextField(20);
 
 
     public UserSignup() {
@@ -18,13 +25,50 @@ public class UserSignup extends JFrame implements ActionListener {
         
         setResizable(false);
         setLocationRelativeTo(null);
-        
-        p.add(name) ;
-        p.add(governement) ;
-        p.add(email) ;
-        p.add(password) ;
+        p.setLayout(new GridBagLayout());
+
+
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        p.add(name,gbc) ;
+        nameField.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        p.add(nameField, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 3;
+        p.add(governement,gbc) ;
+        governementField.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        gbc.gridx = 2;
+        gbc.gridy = 4;
+        p.add(governementField, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 5;
+        p.add(email,gbc) ;
+        emailField.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        gbc.gridx = 2;
+        gbc.gridy = 6;
+        p.add(emailField, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 7;
+        p.add(password,gbc) ;
+        passwordField.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        gbc.gridx = 2;
+        gbc.gridy = 8;
+        p.add(passwordField, gbc);
+
         setContentPane(p) ;
         setVisible(true);
+        name.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 25));
+        governement.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 25));
+        email.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 25));
+        password.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 25));
+
+      
+       
 
 
 
